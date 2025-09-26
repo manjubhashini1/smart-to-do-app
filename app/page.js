5422e9d7cd277829039bd4e38f8921d821f4f37c
manjubhashini1 <bhash19.manj@gmail.com>
2025-09-25 23:49:05 -0500

    added scroll, meta name, heading

--- app/page.js
+++ app/page.js
@@ -5,6 +5,7 @@ import TaskList from "./components/TaskList";
 export default function Page() {
   return (
     <main className="flex items-center justify-center flex-col min-h-screen p-4">
+      <p className="text-sm lg:text-2xl mb-5">⭐ Add your To-Do-List below !!!</p>
       <div className="w-full md:w-1/2">
         <InputBar />
         <TaskList />
